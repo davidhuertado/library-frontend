d3b895259f01bc9ef8d1698eb3445aed3f6b7de8
David Hurtado <davidhuertado@gmail.com>
2022-12-28 16:33:47 +0100

    bug services baseUrl

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const baseUrl = '/api/books';
 //For development
-axios.defaults.baseURL = `http://localhost:3003`;
+// axios.defaults.baseURL = `http://localhost:3003`;
 
 let token: null | string = null;
 
